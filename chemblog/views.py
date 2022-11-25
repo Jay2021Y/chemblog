f53d2bf3bfc2724d853e3fdfe7c3991ce0cea2a7
윤재은 <yokalssam@naver.com>
2022-11-25 13:05:19 +0900

    logger chemblog added

--- chemblog/views.py
+++ chemblog/views.py
@@ -6,10 +6,13 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
+import logging
+
+logger = logging.getLogger('chemblog')
 
 
 def index(request):
-    3/0
+    logger.info('INFO level message')
     page_number = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     post_list = Post.objects.order_by('-pub_date')
